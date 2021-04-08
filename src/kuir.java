0e5da21a709259e0996b75d59f3155418374744c
haandy98 <haandy98@naver.com>
2021-04-08 14:46:43 +0900

    Add Searcher.java, Add -s option in kuir.java

--- src/kuir.java
+++ src/kuir.java
@@ -12,6 +12,7 @@ public class kuir {
         makeCollection makeCollection = new makeCollection();
         makeKeyword makeKeyword = new makeKeyword();
         indexer indexer = new indexer();
+        Searcher searcher = new Searcher();
 
         if (args[0].equals("-c")) {
             path = args[1];
@@ -20,9 +21,19 @@ public class kuir {
         } else if (args[0].equals("-k")) {
             path = args[1];
             makeKeyword.makeKeyword(path);
-        } else if (args[0].equals("-i")){
+
+        } else if (args[0].equals("-i")) {
             path = args[1];
             indexer.indexer(path);
+
+        } else if (args[0].equals("-s")) {
+            path = args[1];
+            String query = null;
+            if (args[2].equals("-q")) {
+                query = args[3];
+            }
+            searcher.Searcher(path, query);
+
         } else {
             System.out.println("Wrong argument!");
         }
